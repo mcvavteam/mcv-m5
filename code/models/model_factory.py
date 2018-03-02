7ae4295d15eed9d71a514c18c6e1cb7562eed23e
mkmenta <mkmenta95@gmail.com>
2018-03-02 22:52:46 +0100

    Added SEResNet50 to model factory option list

--- code/models/model_factory.py
+++ code/models/model_factory.py
@@ -76,7 +76,7 @@ class Model_Factory():
     def make(self, cf, optimizer=None):
         if cf.model_name in ['lenet', 'alexNet', 'vgg16', 'vgg19', 'resnet50',
                              'InceptionV3', 'fcn8', 'unet', 'segnet',
-                             'segnet_basic', 'resnetFCN', 'yolo', 'tiny-yolo']:
+                             'segnet_basic', 'resnetFCN', 'yolo', 'tiny-yolo','se_resnet50']:
             if optimizer is None:
                 raise ValueError('optimizer can not be None')
 
