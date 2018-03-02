d7cf24fb0b6b2522fdbb79fa40a5837206c91c65
mkmenta <mkmenta95@gmail.com>
2018-03-02 23:32:27 +0100

    Set test_model to True

--- code/config/tt100k_classif.py
+++ code/config/tt100k_classif.py
@@ -14,7 +14,7 @@ weights_file                 = 'weights.hdf5'  # Training weight file name
 
 # Parameters
 train_model                  = True            # Train the model
-test_model                   = False           # Test the model
+test_model                   = True            # Test the model
 pred_model                   = False           # Predict using the model
 
 # Debug
