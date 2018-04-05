4c6baa3760914cf4890ccd46bad18e3a70b5d6b6
mkmenta <mkmenta95@gmail.com>
2018-04-05 16:29:49 +0200

    No resize

--- code/config/segmentation/kitti_segmentation.py
+++ code/config/segmentation/kitti_segmentation.py
@@ -31,9 +31,9 @@ batch_size_test              = 10              # Batch size during testing
 crop_size_train              = None            # Crop size during training (Height, Width) or None
 crop_size_valid              = None            # Crop size during validation
 crop_size_test               = None            # Crop size during testing
-resize_train                 = (360, 480)      # Resize the image during training (Height, Width) or None
-resize_valid                 = (360, 480)      # Resize the image during validation
-resize_test                  = (360, 480)      # Resize the image during testing
+resize_train                 = None      # Resize the image during training (Height, Width) or None
+resize_valid                 = None      # Resize the image during validation
+resize_test                  = None      # Resize the image during testing
 
 # Data shuffle
 shuffle_train                = True            # Whether to shuffle the training data
